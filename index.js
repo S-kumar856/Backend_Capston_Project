88ce79326628bfe1a899d66c9b28393076e9fb55
S KUMAR <hk856856@gmail.com>
2024-12-05 12:51:56 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose")
+const userRoute = require('./routes/user')
+const bodyParser = require('body-parser')
+dotenv.config();
+const port = process.env.PORT || 3000;
+const path = require("path");
+
+app.use(express.static(path.join(__dirname,"public")))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "public","try.html"));
+    // res.send('Hello World!')
+});
+
+// middelwares & routers
+app.use(express.urlencoded({ extended:true }));
+app.use(bodyParser.json())
+app.use('/api/user', userRoute)
+
+app.listen(port, () => {
+    mongoose.connect(process.env.MONGODB_URL)
+    .then(()=>{
+        console.log("Connected to MongoDB")
+    }).catch((err) => {
+        console.log(err);
+    })
+    console.log(`Server is running on port ${port}`);
+});
+
